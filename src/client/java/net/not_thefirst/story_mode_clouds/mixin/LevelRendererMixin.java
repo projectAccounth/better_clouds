dad540bde918d8de53cb6d41d6fefdaf8e2312b8
not_thefirst <aquaticclone@gmail.com>
2025-09-08 03:52:11 +0100

    Cloud rendered toggle update

--- src/client/java/net/not_thefirst/story_mode_clouds/mixin/LevelRendererMixin.java
+++ src/client/java/net/not_thefirst/story_mode_clouds/mixin/LevelRendererMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.phys.Vec3;
 import net.not_thefirst.story_mode_clouds.compat.Compat;
+import net.not_thefirst.story_mode_clouds.config.CloudsConfiguration;
 import net.not_thefirst.story_mode_clouds.renderer.CustomCloudRenderer;
 
 import org.spongepowered.asm.mixin.Final;
@@ -85,6 +86,8 @@ public abstract class LevelRendererMixin {
         if (this.cloudRenderer == null) {
             this.cloudRenderer = new CustomCloudRenderer();
         }
+
+        if (!CloudsConfiguration.INSTANCE.CLOUDS_RENDERED) return;
 
         this.cloudRenderer.render(cloudColor, status, cloudHeight, vec3, partialTicks);
     }
